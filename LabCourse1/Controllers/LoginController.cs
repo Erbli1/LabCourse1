f8bfb3acc19c35da9633085e48510f394034b740
Erblin Gashi <eg52040@ubt-uni.net>
2022-05-04 01:38:48 +0200

    Login Form Start

---
+++ LabCourse1/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using LabCourse1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabCourse1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if (userModel.UserName == "Erblin Gashi" && userModel.Password == "12345")
+            { 
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                return View("LoginFailed", userModel);
+            }
+        }
+    }
+}
---
+++ LabCourse1/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LabCourse1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name="Erblin", int secretNumber=99)
+        {
+            ViewBag.Name = name;
+            ViewBag.SecretNumber = secretNumber;
+            return View();
+        }
+    }
+}
