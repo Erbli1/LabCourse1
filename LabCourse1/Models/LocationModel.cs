19a56d996545d5661ba99b723a3c75b8e270f57b
Erblin Gashi <93210523+Erbli1@users.noreply.github.com>
2022-05-04 10:24:51 +0200

    Delete LocationModel.cs

--- LabCourse1/Models/LocationModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LabCourse1.Models
-{
-    public class LocationModel
-    {
-        public string Shteti { get; set; }
-        public string Qyteti { get; set; }
-        public int ZipKodi { get; set; }
-    }
-}
